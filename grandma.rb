244f2016a9fd2778bfb329febead6f9199f71ea5
Jesse Reeves <reeveswriting@gmail.com>
2019-02-11 04:37:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
-    "HUH?! SPEAK UP, SONNY!"
-  END
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
   
